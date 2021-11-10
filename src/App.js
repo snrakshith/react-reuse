1232b20acd14d2bee1dc9e874dcbfa9d6a4a2949
Rakshith <snrakshith.97@gmail.com>
2021-11-10 17:32:52 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import FormInput from "./components/FormInput";
+
+const App = () => {
+  return (
+    <div className="App">
+      <form>
+        <FormInput />
+      </form>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/FormInput.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const FormInput = () => {
+  return (
+    <div>
+      <h1>gh</h1>
+    </div>
+  );
+};
+
+export default FormInput;
